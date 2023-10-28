9311111688b6a55763160594e73e6e20f3d4f724
Jacob Pudwill <jacobpudwill@gmail.com>
2023-10-28 16:50:13 -0500

    Create a function to generate markdown for README.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,7 +11,7 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== 'None') {
-    return `* [License](#license)`
+    return `- [License](#license)`
   }
   return '';
 }
@@ -31,7 +31,62 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}`;
+  return `
+  # ${data.title}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+
+  - [Usage](#usage)
+
+  ${renderLicenseLink(data.license)}
+
+  - [Contributing](#contributing)
+
+  - [Tests](#tests)
+
+  - [Questions](#questions)
+
+  ## Installation
+
+  To install necassary dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Badges
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ## Questions
+
+  If you have any questions about the repository, you can reach out to me at ${data.email}.
+  For more of my work visit [${data.github}](https://github.com/${data.github}/)
+  `;
 }
 
 module.exports = generateMarkdown;
